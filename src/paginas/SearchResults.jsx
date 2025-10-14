00ab088eb86f1d0828917f785b36cdfad9bcd83e
matissepal <20232147@aloe.ulima.edu.pe>
2025-10-13 21:50:39 -0500

    a ver

---
+++ src/paginas/SearchResults.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SearchResults() {
+  return (
+    <section className="card">
+      <h1>Resultados de búsqueda</h1>
+      <p>Aquí aparecerán los productos encontrados 🐾</p>
+    </section>
+  )
+}
